61f63bc3fd48888875953bb90be124034138a251
minaGorge1 <minag2077@gmail.com>
2023-06-15 22:42:31 +0300

    Update index.router.js

--- src/index.router.js
+++ src/index.router.js
@@ -5,17 +5,12 @@ import { globalErrorHandling } from "./utils/errorHandling.js";
 import foodRouter from "./modules/food/food.route.js";
 import userRouter from "./modules/user/user.route.js";
 
-import path from "path";
-import { fileURLToPath } from "url";
 
-const __dirname = path.dirname(fileURLToPath(import.meta.url)) // url a7na fiin 7alin
 
 
 const initApp = (app, express) => {
     app.use(cors())
     app.use(express.json({}))
-
-    app.use("/uploads", express.static(path.join(__dirname, "./uploads")))
 
     app.get("/", (req, res, next) => {
         res.json("home")
